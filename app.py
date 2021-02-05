16eab07a114c11820f9d0a302c0efe3bd744804b
Sherryll Elliott <sherryllelliott@gmail.com>
2021-02-05 20:29:50 +0000

    get close match used to catch close spellings

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import json
+from difflib import get_close_matches
+
+
 data = json.load(open("data.json"))
 
 def translate(word):
   word = word.lower()
   if word in data:
     return data[word]
-
+  elif len(get_close_matches(word, data.keys())) > 0:
+    return "did you mean %s instead?" % get_close_matches(word, data.keys())[0]
   else:
     return "The word doesn't exist"
-  
 
 user_word = input("Enter word: ")
 
